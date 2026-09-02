19b323c148a6796243264ca95e384b5043c72175
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-09-02 15:47:46 +0700

    style(dart): format transport API

--- packages/ggwave_dart/lib/src/transport.dart
+++ packages/ggwave_dart/lib/src/transport.dart
@@ -6,8 +6,14 @@ abstract interface class GgWaveTransport {
   Stream<Uint8List> get messages;
   Future<void> initialize();
   Future<void> setUltrasonicFrequency(double hz);
-  Future<Float32List> encode(Uint8List data, {GgWaveProtocol protocol = GgWaveProtocol.audibleFast, int volume = 60});
+  Future<Float32List> encode(
+    Uint8List data, {
+    GgWaveProtocol protocol = GgWaveProtocol.audibleFast,
+    int volume = 60,
+  });
   Future<void> play(Float32List waveform);
-  Future<void> startListening({GgWaveProtocol protocol = GgWaveProtocol.audibleFast});
+  Future<void> startListening({
+    GgWaveProtocol protocol = GgWaveProtocol.audibleFast,
+  });
   Future<void> stopListening();
 }
