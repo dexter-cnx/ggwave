8074bb4055c669e0dc64b0f2c8050e89466bee5c
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-09-02 13:22:54 +0700

    style(rust): apply rustfmt to Flutter native entrypoint

--- packages/ggwave_flutter/rust/src/lib.rs
+++ packages/ggwave_flutter/rust/src/lib.rs
@@ -2,4 +2,6 @@ mod api;
 pub use api::*;
 use flutter_rust_bridge::frb;
 #[frb(init)]
-pub fn init_app() { flutter_rust_bridge::setup_default_user_utils(); }
+pub fn init_app() {
+    flutter_rust_bridge::setup_default_user_utils();
+}
