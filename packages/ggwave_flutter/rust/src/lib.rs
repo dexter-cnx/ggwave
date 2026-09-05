e6a43b1f3b4d823d9b10594150e63f47fb80e081
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-09-05 20:22:35 +0700

    style(rust): satisfy rustfmt after Oboe TX

--- packages/ggwave_flutter/rust/src/lib.rs
+++ packages/ggwave_flutter/rust/src/lib.rs
@@ -1,5 +1,5 @@
-mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 mod api;
+mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 pub use api::*;
 use flutter_rust_bridge::frb;
 
